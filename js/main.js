9d10bd1366755f203e2995da37b74a86e09e9b37
donialajimi1996 <donialajimi@yahoo.fr>
2018-12-19 11:42:33 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function facebook() {
+    window.open("https://www.facebook.com/donia.laajimi.1");
+  }
+  function instagram() {
+    window.open("https://www.instagram.com/laajimidonia/");
+  }
+  function github() {
+    window.open("https://github.com/donialajimi1996");
+  }
+  
