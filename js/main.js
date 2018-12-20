3116970755099e99e159598eeb0997ac0c5be0b5
donialajimi1996 <donialajimi@yahoo.fr>
2018-12-20 16:29:44 +0100

    ends commit

--- js/main.js
+++ js/main.js
@@ -8,3 +8,21 @@ function facebook() {
     window.open("https://github.com/donialajimi1996");
   }
   
+  function basichtml() {
+    window.open("../basic_html/index1.html");
+  }
+  function basiccss() {
+    window.open("../basic_css/index.html");
+  }
+  function basicjs() {
+    window.open("../basic javascript/index.html");
+  }
+  function basicjq() {
+    window.open("../basic jquery/index.html");
+  }
+  function htmlform() {
+    window.open("../html forms/index.html");
+  }
+  function timer() {
+    window.open("../timer/timer.html");
+  }
